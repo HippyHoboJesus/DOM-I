e74c11b85e85909f76079c644758a0f56f20d6c2
Jason Greer <jasemgreer@gmail.com>
2023-05-10 10:47:21 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -39,4 +39,53 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
+const navBar = document.querySelectorAll("nav a");
+navBar[0].textContent = "Services"
+navBar[1].textContent = "Product"
+navBar[2].textContent = "Vision"
+navBar[3].textContent = "Features"
+navBar[4].textContent = "About"
+navBar[5].textContent = "Contact"
+navBar.forEach(item => item.className = "italic")
+
+const logo = document.querySelector(".logo")
+logo.setAttribute('src', "http://localhost:9000/img/logo.png")
+
+const ctaImg = document.querySelector("#cta-img")
+ctaImg.setAttribute('src', "http://localhost:9000/img/cta.png")
+
+const middle = document.querySelector(".middle-img")
+middle.setAttribute('src', "http://localhost:9000/img/accent.png")
+
+const h1 = document.querySelector("h1")
+h1.textContent = "DOM Is Awesome"
+
+const button = document.querySelector("button");
+button.textContent = "Get Started"
+
+const h4 = document.querySelectorAll("h4");
+h4[0].textContent = "Features"
+h4[1].textContent = "About"
+h4[2].textContent = "Services"
+h4[3].textContent = "Product"
+h4[4].textContent = "Vision"
+h4[5].textContent = "Contact"
+
+const p = document.querySelectorAll("p")
+p[0].textContent ="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[1].textContent ="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[2].textContent ="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[3].textContent ="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[4].textContent ="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+p[5].textContent ="123 Way 456 Street Somewhere, USA"
+p[6].textContent ="1 (888) 888-8888"
+p[7].textContent ="sales@greatidea.io"
+
+const copyright = document.querySelector( "footer a" ) 
+copyright.textContent = "Copyright Great Idea! 2021"
+copyright.className = "bold"
+
+
+
 console.log('project wired!')
+
